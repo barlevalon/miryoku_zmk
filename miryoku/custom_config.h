6b1c40be619749777cd737e61c63a78bebfd4d15
Alon Hearter <alon.hearter@gmail.com>
2024-02-01 17:41:17 +0200

    add EP_ON to both sides

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,6 +2,7 @@
 // https://github.com/manna-harbour/miryoku
 
 #define XXX &none
+#define EPN &ext_power EP_ON
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -11,7 +12,7 @@
 ) \
 &kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
     XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-    XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+    EPN  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  EPN \
                    K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
