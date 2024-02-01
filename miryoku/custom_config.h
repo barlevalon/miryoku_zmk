8a2b01b39505bd63cb687dddaab7556fef7778e8
Alon Hearter <alon.hearter@gmail.com>
2024-02-01 20:06:45 +0200

    oops

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,7 +12,7 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
+    XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
    CAPS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  RAY \
     XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  TBS \
                    K32  K33  K34       K35  K36  K37
