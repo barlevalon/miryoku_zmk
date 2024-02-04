a17424e4ca2dbf53b623350001c43a38edafb5bd
Alon Hearter <alon.hearter@gmail.com>
2024-02-04 10:56:33 +0200

    ordering

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,23 +6,6 @@
 #define RAY &kp LG(SPACE)
 #define TBS &kp RG(RA(RC(T)))
 
-#define MIRYOKU_LAYOUTMAPPING_CORNE( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-&to U_GAME  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
-&kp   CAPS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  RAY \
-       XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  TBS \
-                      K32  K33  K34       K35  K36  K37
-
-#define MIRYOKU_LAYER_GAME \
-&kp    TAB,  &kp Q,  &kp W,     &kp E,     &kp R,      &kp T,          &kp Y,    &kp U,   &kp I,    &kp O,  &kp P,    &to U_BASE,  \
-&kp  LCTRL,  &kp A,  &kp S,     &kp D,     &kp F,      &kp G,          &kp H,    &kp J,   &kp K,    &kp L,  &kp DQT,  XXX,   \
-&kp LSHIFT,  &kp Z,  &kp X,     &kp C,     &kp V,      &kp B,          &kp N,    &kp M,   &kp LT,   &kp GT, &kp FSLH, XXX,  \
-             XXX,   XXX,        &kp SPACE, &kp LALT,   &kp M,          &kp RET,  &kp ESC, &kp DEL,  XXX,    XXX
-
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
 MIRYOKU_X(EXTRA,  "Extra") \
@@ -47,3 +30,21 @@ MIRYOKU_X(GAME,   "Game")
 #define U_SYM    8
 #define U_FUN    9
 #define U_GAME   10
+
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&to U_GAME  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
+&kp   CAPS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  RAY \
+       XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  TBS \
+                      K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_LAYER_GAME \
+&kp    TAB,  &kp Q,  &kp W,     &kp E,     &kp R,      &kp T,          &kp Y,    &kp U,   &kp I,    &kp O,  &kp P,    &to U_BASE,  \
+&kp  LCTRL,  &kp A,  &kp S,     &kp D,     &kp F,      &kp G,          &kp H,    &kp J,   &kp K,    &kp L,  &kp DQT,  XXX,   \
+&kp LSHIFT,  &kp Z,  &kp X,     &kp C,     &kp V,      &kp B,          &kp N,    &kp M,   &kp LT,   &kp GT, &kp FSLH, XXX,  \
+             XXX,   XXX,        &kp SPACE, &kp LALT,   &kp M,          &kp RET,  &kp ESC, &kp DEL,  XXX,    XXX
+
