d97340a1943ba89e275d4b1415eb2995f245cae6
Alon Hearter <alon.hearter@gmail.com>
2024-02-04 15:29:41 +0200

    game layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -49,12 +49,12 @@ MIRYOKU_X(GAME,   "Game")
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 &kp TAB     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &to U_BASE \
-&kp LCTRL   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+&kp LCTRL   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp ESC \
 &kp LSHIFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                       K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYER_GAME \
-&kp Q,  &kp W,     &kp E,     &kp R,      &kp T,          &kp Y,    &kp U,   &kp I,      &kp O,    &kp P, \
-&kp A,  &kp S,     &kp D,     &kp F,      &kp G,          &kp H,    &kp J,   &kp K,      &kp L,    &kp DQT, \
-&kp Z,  &kp X,     &kp C,     &kp V,      &kp B,          &kp N,    &kp M,   &kp COMMA,  &kp DOT,  &kp FSLH, \
-  XXX,    XXX,     &kp SPACE, &kp LALT,   &kp M,          &kp RET,  &kp ESC, &kp DEL,    XXX,      XXX
+&kp Q,  &kp W,     &kp E,     &kp R,      &kp T,          &kp Y,    &kp U,    &kp I,      &kp O,    &kp P, \
+&kp A,  &kp S,     &kp D,     &kp F,      &kp G,          &kp H,    &kp J,    &kp K,      &kp L,    &kp DQT, \
+&kp Z,  &kp X,     &kp C,     &kp V,      &kp B,          &kp N,    &kp M,    &kp COMMA,  &kp DOT,  &kp FSLH, \
+  XXX,    XXX,     &kp SPACE, &kp LALT,   &kp M,          &kp RET,  &kp BSPC, &kp DEL,    XXX,      XXX
