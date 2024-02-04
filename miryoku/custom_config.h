9478a3ced208effa7426a458cf0efe1052b94a44
Alon Hearter <alon.hearter@gmail.com>
2024-02-04 10:52:57 +0200

    game layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,8 +12,38 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
-&kp CAPS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  RAY \
-     XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  TBS \
-                    K32  K33  K34       K35  K36  K37
+&to U_GAME  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
+&kp   CAPS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  RAY \
+       XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  TBS \
+                      K32  K33  K34       K35  K36  K37
 
+#define MIRYOKU_LAYER_GAME \
+&kp    TAB,  &kp Q,  &kp W,     &kp E,     &kp R,      &kp T,          &kp Y,    &kp U,   &kp I,    &kp O,  &kp P,    &to U_BASE,  \
+&kp  LCTRL,  &kp A,  &kp S,     &kp D,     &kp F,      &kp G,          &kp H,    &kp J,   &kp K,    &kp L,  &kp DQT,  XXX,   \
+&kp LSHIFT,  &kp Z,  &kp X,     &kp C,     &kp V,      &kp B,          &kp N,    &kp M,   &kp LT,   &kp GT, &kp FSLH, XXX,  \
+             XXX,   XXX,        &kp SPACE, &kp LALT,   &kp M,          &kp RET,  &kp ESC, &kp DEL,  XXX,    XXX
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(GAME,   "Game")
+
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+#define U_GAME   10
