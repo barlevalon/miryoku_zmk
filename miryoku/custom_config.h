840604922707099a75ac98722f12b0487ce2b6ab
Alon Hearter <alon.hearter@gmail.com>
2024-02-12 13:46:50 +0200

    &tog?

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -37,7 +37,7 @@ MIRYOKU_X(GAME,   "Game")
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&to U_GAME  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
+&tog U_GAME  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  PWD \
 &kp   CAPS  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  RAY \
        XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  TBS \
                       K32  K33  K34       K35  K36  K37
@@ -48,7 +48,7 @@ MIRYOKU_X(GAME,   "Game")
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp TAB     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &to U_BASE \
+&kp TAB     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &tog U_GAME \
 &kp LCTRL   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp ESC \
 &kp LSHIFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                       K32  K33  K34       K35  K36  K37
